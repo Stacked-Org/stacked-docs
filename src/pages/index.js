69a86a0905fda7091e478edecf834adbb4a34064
Dane Mackier <dane.mackier@outlook.com>
2023-01-10 09:51:46 +0200

    * Adds tailwindcss * Updates landing page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,17 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+    <header className="bg-white">
+      <div className="container mx-auto text-center py-24 px-64">
+      <h1 class="text-5xl md:text-8xl font-extrabold leading-tighter tracking-tighter mb-4 aos-init aos-animate" data-aos="zoom-y-out">The <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">production</span><h1>App framework</h1></h1>
+        <p className="text-3xl py-2 text-gray-500 px-48">{siteConfig.tagline}</p>
+
+        <div className="py-10">
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started/overview">
-            Start Learning
+            className="bg-black rounded-md text-white px-16 py-4 text-xl font-bold"
+            to="/docs/getting-started/overview"
+          >
+            Get Started
           </Link>
         </div>
       </div>
