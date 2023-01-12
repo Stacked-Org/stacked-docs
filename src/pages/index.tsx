195fca424fe4e6a4e2972b627292997b9fbf20da
Shreyas Mididoddi <shreyassanthu77@gmail.com>
2023-01-12 14:46:26 +0530

    removed header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,20 @@
 import React, { useCallback, useState } from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import { CopyIcon, GithubIcon } from "../components/Icons";
 import { motion } from "framer-motion";
-
-import { ThemeConfig } from "@docusaurus/preset-classic";
-import Features from "../components/Features";
 
 export default function Home() {
   const {
-    siteConfig: { themeConfig, tagline },
+    siteConfig: { tagline },
   } = useDocusaurusContext();
-  const headerImg = (themeConfig as ThemeConfig).navbar!.logo!;
 
   return (
     <>
-      <header className="sticky top-0 flex items-center justify-between px-6 py-4 shadow bg-white/20 backdrop-blur-md">
-        <div className="flex items-center pointer-events-none select-none">
-          <img className="h-8" src={headerImg.src} alt={headerImg.alt} />
-          <strong className="pl-4 text-xl">Stacked</strong>
-        </div>
-
-        <a
-          href="https://github.com/Filledstacks/stacked"
-          target="_blank"
-          className="flex items-center px-4 py-2 text-lg text-black transition-all bg-gray-100 rounded-full outline-none ring-transparent focus:ring-purple-500 ring hover:no-underline hover:text-black hover:bg-gray-200"
-        >
-          <GithubIcon className="mr-4" />
-          Github
-        </a>
-      </header>
       <main className="px-6 py-1 min-h-[80vh] flex flex-col items-center justify-center">
         <motion.h1
           className="text-5xl font-black text-center md:text-7xl"
           initial={{ y: 100, opacity: 0 }}
-          whileInView={{ y: 0, opacity: 1 }}
+          animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, type: "spring", bounce: 0.5 }}
         >
           The{" "}
@@ -46,7 +26,7 @@ export default function Home() {
         </motion.h1>
         <motion.div
           initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
+          animate={{ opacity: 1 }}
           transition={{ duration: 0.6 }}
           className="flex flex-col items-center"
         >
